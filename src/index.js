f440fb368496d88451e9b21deb06424501efc28d
Ravinder Mahajan <ravindermahajan890@gmail.com>
2019-03-23 14:44:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ListPage from "./ListPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://graphql-server.ravindermahajan890.now.sh/graphql"
+  }),
+  cache: new InMemoryCache()
+});
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path="/" component={ListPage} />
+      </div>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
