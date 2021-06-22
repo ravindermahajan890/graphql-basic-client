d906df6feb16181aede9ba6053019a5aa0044e5c
RAVINDER MAHAJAN <ravinder.mahajan@telekom-digitial.com>
2021-06-22 16:15:36 +0530

    migration to hooks

--- src/AddMore.js
+++ src/AddMore.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
 import DatePicker from "react-datepicker";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import "react-datepicker/dist/react-datepicker.css";
 const ADD_TODO = gql`
   mutation post($task: String!, $detail: String!) {
@@ -15,65 +14,47 @@ const ADD_TODO = gql`
 const AddMore = () => {
   const [task, setTask] = useState("");
   const [detail, setDetail] = useState(new Date());
-  return (
-    <Mutation mutation={ADD_TODO}>
-      {(addTodo, { data, loading }) => {
-        if (loading)
-          return (
-            <div className="jumbotron jumbotron-fluid">
-              <div className="d-flex justify-content-center">
-                <div
-                  className="spinner-border"
-                  style={{ width: "10rem", height: "10rem" }}
-                  role="status"
-                >
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            </div>
-          );
-        return (
-          <div>
-            <div className="jumbotron">
-              <h1 className="display-4">Please add your Tasks</h1>
-              <form>
-                <div className="form-group">
-                  <label for="formGroupExampleInput">Task</label>
+  const [addTodo] = useMutation(ADD_TODO);
 
-                  <input
-                    className="form-control form-control-lg"
-                    type="text"
-                    placeholder="Enter Task"
-                    onChange={e => {
-                      setTask(e.target.value);
-                    }}
-                  />
-                </div>
-                <div className="form-group">
-                  <label for="formGroupExampleInput2">Date</label>
-                  <DatePicker
-                    className="form-control form-control-lg"
-                    selected={new Date(detail)}
-                    onChange={value => {
-                      setDetail(new Date(value).toDateString());
-                    }}
-                  />
-                </div>
-                <button
-                  type="button"
-                  className="btn btn-primary btn-block"
-                  onClick={async () => {
-                    addTodo({ variables: { task, detail } });
-                  }}
-                >
-                  Add
-                </button>
-              </form>
-            </div>
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">Please add your Tasks</h1>
+        <form>
+          <div className="form-group">
+            <label for="formGroupExampleInput">Task</label>
+
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Enter Task"
+              onChange={(e) => {
+                setTask(e.target.value);
+              }}
+            />
           </div>
-        );
-      }}
-    </Mutation>
+          <div className="form-group">
+            <label for="formGroupExampleInput2">Date</label>
+            <DatePicker
+              className="form-control form-control-lg"
+              selected={new Date(detail)}
+              onChange={(value) => {
+                setDetail(new Date(value).toDateString());
+              }}
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary btn-block"
+            onClick={async () => {
+              addTodo({ variables: { task, detail } });
+            }}
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 export default AddMore;
