163b2fa8c1eeae3a135483fee6fa2f02199f55bf
RAVINDER MAHAJAN <ravinder.mahajan@telekom-digitial.com>
2021-06-22 16:58:23 +0530

    migration to hooks

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+const GET_DATA = gql`
+  query fetch {
+    fetch {
+      task
+      detail
+      taskId
+      pending
+    }
+  }
+`;
+
 const DELETE_TASK_QUERY = gql`
   mutation delete($taskId: String!) {
     delete(taskId: $taskId) {
@@ -13,90 +23,84 @@ const DELETE_TASK_QUERY = gql`
   }
 `;
 
-const deleteTask = async (taskId, deleteTaskQuery) => {
-  deleteTaskQuery({ variables: { taskId } });
-};
-const ExchangeRates = () => (
-  <div>
-    <Query
-      query={gql`
-        query fetch {
-          fetch {
-            task
-            detail
-            taskId
-            pending
-          }
-        }
-      `}
-    >
-      {({ loading, error, data }) => {
-        if (loading)
-          return (
-            <div className="jumbotron jumbotron-fluid">
-              <div className="d-flex justify-content-center">
-                <div
-                  className="spinner-border"
-                  style={{ width: "10rem", height: "10rem" }}
-                  role="status"
-                >
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            </div>
-          );
-        if (error) return <p>Error :(</p>;
+const UNDO_TASK_QUERY = gql`
+  mutation undo($taskId: String!) {
+    undo(taskId: $taskId) {
+      task
+      detail
+      taskId
+      pending
+    }
+  }
+`;
 
-        return (
-          <div className="list-group">
-            {data.fetch.map(
-              ({ task, detail, taskId, pending: taskPending }, index) => {
-                return (
-                  <Mutation mutation={DELETE_TASK_QUERY}>
-                    {(
-                      deleteTaskQuery,
-                      { data: { delete: { pending = true } = {} } = {} }
-                    ) => (
-                      <a
-                        href="#"
-                        className={`list-group-item list-group-item-action ${
-                          !taskPending || !pending ? "opaque" : ""
-                        }`}
-                        key={index}
-                      >
-                        <div className={`d-flex w-100 justify-content-between`}>
-                          {<h5 className="col-sm-8">{task}</h5>}
-                          <button
-                            className="btn btn-success col-sm-offset-2
-                      col-sm-2"
-                            onClick={async () => {
-                              await deleteTask(taskId, deleteTaskQuery);
-                            }}
-                          >
-                            done
-                          </button>
-                          <button
-                            className="btn btn-warning col-sm-offset-2
-                      col-sm-2"
-                            onClick={async () => {
-                              await deleteTask(taskId, deleteTaskQuery);
-                            }}
-                          >
-                            Clear
-                          </button>
-                          <small />
-                        </div>
-                        <p className="mb-1 col-sm-12">{detail}</p>
-                      </a>
-                    )}
-                  </Mutation>
-                );
-              }
-            )}
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(GET_DATA);
+  const [deleteTask] = useMutation(DELETE_TASK_QUERY, {
+    refetchQueries: [{ query: GET_DATA }],
+  });
+  const [undoTask] = useMutation(UNDO_TASK_QUERY, {
+    refetchQueries: [{ query: GET_DATA }],
+  });
+  if (loading)
+    return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="d-flex justify-content-center">
+          <div
+            className="spinner-border"
+            style={{ width: "10rem", height: "10rem" }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
           </div>
-        );
-      }}
-    </Query>
-  </div>
-);
+        </div>
+      </div>
+    );
+  return (
+    <div className="list-group">
+      {data.fetch.map(
+        ({ task, detail, taskId, pending: taskPending }, index) => {
+          return (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a
+              href="#"
+              className={`list-group-item list-group-item-action ${
+                !taskPending ? "opaque" : ""
+              }`}
+              key={index}
+            >
+              <div className={`d-flex w-100 justify-content-between`}>
+                {<h5 className="col-sm-8">{task}</h5>}
+                {taskPending && (
+                  <button
+                    className="btn btn-success col-sm-offset-2
+                      col-sm-2"
+                    onClick={async () => {
+                      await deleteTask({ variables: { taskId } });
+                    }}
+                  >
+                    done
+                  </button>
+                )}
+                {!taskPending && (
+                  <button
+                    className="btn btn-success col-sm-offset-2
+                      col-sm-2"
+                    onClick={async () => {
+                      await undoTask({ variables: { taskId } });
+                    }}
+                  >
+                    Undo
+                  </button>
+                )}
+                <small />
+              </div>
+              <p className="mb-1 col-sm-12">{detail}</p>
+            </a>
+          );
+        }
+      )}
+    </div>
+  );
+};
 export default ExchangeRates;
--- src/index.js
+++ src/index.js
@@ -3,28 +3,26 @@ import ReactDOM from "react-dom";
 import ListPage from "./ListPage";
 import AddPage from "./AddMore";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 import { Link } from "react-router-dom";
 
 const defaultOptions = {
   watchQuery: {
-    fetchPolicy: "network-only",
-    errorPolicy: "ignore"
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
   },
   query: {
-    fetchPolicy: "network-only",
-    errorPolicy: "all"
-  }
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
 };
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://api.ravindermahajan.co.in/graphql"
+    uri: "https://api.ravindermahajan.co.in/graphql",
   }),
   cache: new InMemoryCache(),
-  defaultOptions
+  defaultOptions,
 });
 ReactDOM.render(
   <ApolloProvider client={client}>
